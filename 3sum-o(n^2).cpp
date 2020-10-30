3e6a86fc25675033c3f77ef5b4343bf04642d6f7
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-10-31 02:18:23 +0530

    programs added

---
+++ 3sum-o(n^2).cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<vector>
+class Solution {
+public:
+    std::vector<vector<int>> threeSum(std::vector<int>& nums) {
+        int l= nums.size(),i,j;
+        set<vector<int>> fin;
+        vector<int>demo;
+        map<int,int> dict;
+        for (i=0;i<l;i++){
+            int sum= 0-nums[i];
+            for (j=i+1;j<l;j++){
+                int target=sum-nums[j];
+                if (dict.find(target) == dict.end()){
+                    dict[nums[j]]=j;
+                }
+                else{
+                    auto itr=dict.find(target);
+                    demo.push_back(nums[i]);
+                    demo.push_back(nums[j]);
+                    demo.push_back(nums[itr->second]);
+                    sort(demo.begin(),demo.begin()+3);
+                    fin.insert(demo);
+                    demo.clear();
+                }
+            }
+            dict.clear();
+        }
+        vector<vector<int>> ans(fin.begin(),fin.end());
+        return ans;
+    }
+};
---
+++ removeduplicatesSarray.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<vector>
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        int p,q,s;
+        int l = nums.size();
+        if (l==0){
+            return 0;
+        }
+        int ans=1;
+        q=0;
+        p=1;
+        while (p!=l){
+            if (nums[q]==nums[p]){
+               p+=1;
+            }
+            else{
+                q+=1;
+                ans+=1;
+                nums[q]=nums[p];
+            }
+        }
+        for (p=l-1;p>ans;p--){
+            nums.erase(nums.begin()+p);
+        }
+        return ans;
+    }
+};
